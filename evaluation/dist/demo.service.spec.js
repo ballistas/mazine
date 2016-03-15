5a5b36c1b881f8164e73e450e8448ab2eaf733b0
ballista <flow_star#77>
2016-03-15 20:23:43 +0100

    working tests

--- evaluation/dist/demo.service.spec.js
+++ evaluation/dist/demo.service.spec.js
@@ -1,11 +1,15 @@
 "use strict";
-var demo_service_1 = require("./demo.service");
+const testing_1 = require("angular2/testing");
+const demo_service_1 = require("./demo.service");
+const testing_2 = require("angular2/testing");
 /**
- * Created by pakunert on 11.03.2016.
+ * Created by pakunert on 15.03.2016.
  */
-describe('DemoService', function () {
-    var service = new demo_service_1.DemoService();
-    it('behaviour', function () {
-        expect(service).not.toBeNull();
-    });
+testing_1.describe('demoService', () => {
+    testing_1.beforeEachProviders(() => [
+        demo_service_1.DemoService
+    ]);
+    testing_1.it('should return "value"', testing_2.inject([demo_service_1.DemoService], (service) => {
+        testing_1.expect(service.invokeMethode()).toBe('result');
+    }));
 });
