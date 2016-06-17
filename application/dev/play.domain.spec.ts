b0ebb9251eb29c305b99edf020570d34f3fcb5ae
Patrick Kunert <patrick.kunert@gmail.com>
2016-06-18 00:23:05 +0200

    Tests

--- application/dev/play.domain.spec.ts
+++ application/dev/play.domain.spec.ts
@@ -1,27 +1,75 @@
 ///<reference path="../typings/jasmine/jasmine" />
 
-import * as ng2 from "@angular/core/testing";
+import {
+    beforeEach,
+    beforeEachProviders,
+    afterEach,
+    describe,
+    expect,
+    it,
+    inject
+} from "@angular/core/testing";
 import * as play from "./play.domain";
 
 /**
  * Created by pakunert on 27.04.2016.
  */
 
+export let myMatchers = {
+
+    toExceed100:()=>({
+        compare:value =>{
+
+            let result={
+                pass:false,
+                message:null
+            };
+
+            result.pass = value > 100;
+
+            result.message = `${value} ${result.pass ? '>' : '<='} 100`;
+
+            return result;
+        }
+    })
+};
+
 describe('Maazine',function(){
 
+
+    beforeAll(function () {
+        console.log('initialize suite...')
+    });
+
     beforeEach(function(){
-        console.log('initialize...')
+        console.log('initialize test...')
     });
 
     it('should',function(){
 
-        expect(1).toBe(2);
+        expect(2).toBe(2);
+
+
     });
 });
 
 describe('Scriptservice',function(){
 
-    ng2.beforeEachProviders(function(){
-        
+    let service;
+
+    beforeEachProviders(()=>[play.ScriptService]);
+
+    beforeEach(inject([play.ScriptService],ser=>{
+
+        service = ser;
+    }));
+
+    it('should return 3 plays',()=>{
+
+        let result = service.find();
+
+        expect(result.length).toEqual(3);
     });
 });
+
+
